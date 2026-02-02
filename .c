f530f6d1c5b3e5346fb288908c93d7bf754c6ca4
Anmol  garg <agarg3287@gmail.com>
2026-02-03 00:52:06 +0530

    Add files via upload

---
+++ .c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main (void)  {
+    int age = 22;
+    float pi = 3.14;
+    char star = '*';
+    printf(" age is %d \n" , age );
+    printf(" pi is %f \n" , pi);
+    printf(" star is %c \n" , star);
+    printf(" Hello World \n");
+    printf(" Anmol bro \n");
+}
