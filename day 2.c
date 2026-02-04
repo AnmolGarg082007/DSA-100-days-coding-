e7858b2ec97427e587ba6b382071718a0daece93
Anmol  garg <agarg3287@gmail.com>
2026-02-04 22:13:28 +0530

    Update day 2.c

--- day 2.c
+++ day 2.c
@@ -16,4 +16,15 @@ int main() {
     // Read position to delete (1-based)
     scanf("%d", &pos);
     
-    // Sh
+    // Shift elements to the left
+    for(int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    
+    // Print updated array
+    for(int i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
